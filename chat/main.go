1e532e355572a777ac6bfa0958e2cb3c7581dd29
Shintaro Murakami <mrkm4ntr@gmail.com>
2017-03-22 23:19:53 +0900

    Getting more out of templates

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+	"flag"
 )
 
 type templateHandler struct {
@@ -17,16 +18,19 @@ type templateHandler struct {
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.tmpl = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-		t.tmpl.Execute(w, nil)
+		t.tmpl.Execute(w, r)
 	})
 }
 
 func main() {
+	addr := flag.String("addr", ":8080", "The addr of application")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Start web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
