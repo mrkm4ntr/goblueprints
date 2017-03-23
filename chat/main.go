697c0eddf4ea00211f697a262b0b2a3905dabc4a
Shintaro Murakami <mrkm4ntr@gmail.com>
2017-03-23 13:32:39 +0900

    Fix bug

--- chat/main.go
+++ chat/main.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"sync"
 	"flag"
+	"../trace"
+	"os"
 )
 
 type templateHandler struct {
@@ -18,17 +20,22 @@ type templateHandler struct {
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.tmpl = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-		t.tmpl.Execute(w, r)
 	})
+	t.tmpl.Execute(w, r)
 }
 
 func main() {
 	addr := flag.String("addr", ":8080", "The addr of application")
 	flag.Parse()
+
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
+
 	go r.run()
+
 	log.Println("Start web server on", *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
